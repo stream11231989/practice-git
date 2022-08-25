9ab6e3643031fc8cf675a60cd9ad4191ab32c78a
Bill Lin <stream1123@hotmail.com>
2022-08-26 07:58:25 +0800

    Account CHECK

--- GuestBooks/Models/Guestbooks.cs
+++ GuestBooks/Models/Guestbooks.cs
@@ -9,21 +9,45 @@ namespace GuestBooks.Models
 {
     public class Guestbooks
     {
-        [DisplayName("編號:")]
+        #region for會員系統用資料
+
         public int Id { get; set; }
-        [DisplayName("名字:")]
-        [Required(ErrorMessage ="請輸入名字")]
-        [StringLength(20,ErrorMessage ="名字不可超過20字元")]
+
+        public string Account { get; set; }
+
         public string Name { get; set; }
-        [DisplayName("留言內容")]
-        [Required(ErrorMessage ="請輸入留言內容")]
-        [StringLength(100, ErrorMessage = "留言內容不可超過100字元")]
+
         public string Content { get; set; }
+
         public DateTime CreateTime { get; set; }
+
         public string Reply { get; set; }
-        //DateTime?資料型態，允許有Null產生
-        [DisplayName("回覆時間")]
+
+
         public DateTime? ReplyTime { get; set; }
-        
+        #endregion
+
+
+
+
+
+        #region 單純for留言板用的資料
+        //[DisplayName("編號:")]
+        //public int Id { get; set; }
+        //[DisplayName("名字:")]
+        //[Required(ErrorMessage ="請輸入名字")]
+        //[StringLength(20,ErrorMessage ="名字不可超過20字元")]
+        //public string Name { get; set; }
+        //[DisplayName("留言內容")]
+        //[Required(ErrorMessage ="請輸入留言內容")]
+        //[StringLength(100, ErrorMessage = "留言內容不可超過100字元")]
+        //public string Content { get; set; }
+        //public DateTime CreateTime { get; set; }
+        //public string Reply { get; set; }
+        ////DateTime?資料型態，允許有Null產生
+        //[DisplayName("回覆時間")]
+        //public DateTime? ReplyTime { get; set; }
+        #endregion
+
     }
 }
---
+++ GuestBooks/Services/MembersDBService.cs
@@ -0,0 +1,120 @@
+﻿using GuestBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace GuestBooks.Services
+{
+    public class MembersDBService
+    {
+        //建立與資料庫的連線字串
+        private readonly static string cnstr = ConfigurationManager.ConnectionStrings["ASP.NET MVC"].ConnectionString;
+        //建立與資料庫的連線
+        private readonly SqlConnection conn = new SqlConnection(cnstr);
+        #region 註冊
+        public void Register(Members newMember)
+        {
+            //將密碼HASH過
+            newMember.Password = HashPassword(newMember.Password);
+            //sql新增語法 //IsAdmin預設為0
+            string sql = $@" INSERT INTO Members (Account,Password,Name,Email,Authcode,IsAdmin) VALUES ('{newMember.Account}','{newMember.Password}','{newMember.Name}','{newMember.Email}','{newMember.AuthCode}','0')";
+            //確保程式部會因執行錯誤而中斷
+            try
+            {
+                //開啟資料庫
+                conn.Open();
+                //執行sql指令
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+        #endregion
+
+        #region Hash密碼
+        //Hash密碼用的方法
+        public string HashPassword(string Password)
+        {
+            //宣告Hash時所添加的無意義亂碼值
+            string saltkey = "1q2w3e4r5t6y7u8ui9o0po7tyy";
+            //將剛剛宣告的字串與密碼結合
+            string saltAndPassword = string.Concat(Password, saltkey);
+            //定義SHA256的HASH物件
+            SHA256CryptoServiceProvider sha256Hasher = new SHA256CryptoServiceProvider();
+            //取得密碼轉換成byte資料
+            byte[] PasswordData = Encoding.Default.GetBytes(saltAndPassword);
+            //取得HASH後byte資料
+            byte[] HashDate = sha256Hasher.ComputeHash(PasswordData);
+            //將Hash後byte資料轉成string
+            string Hashresult = Convert.ToBase64String(HashDate);
+            //傳回Hash結果
+            return Hashresult;
+        }
+
+        #endregion
+
+        #region
+        //藉由帳號取得單筆資料的方法
+        private Members GetDataByAccount(string Account)
+        {
+            Members Data = new Members();
+            //sql
+            string sql = $@" select * from Members where Account = {Account} ";
+            //確保程式不會因執行錯誤而中斷
+            try
+            {
+                conn.Open();
+                //執行sql指令
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //取得sql指令
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                Data.Account = dr["Account"].ToString();
+                Data.Password = dr["Password"].ToString();
+                Data.Name = dr["Name"].ToString();
+                Data.Email = dr["Email"].ToString();
+                Data.AuthCode = dr["AuthCode"].ToString();
+                Data.IsAdmin = Convert.ToBoolean(dr["IsAdmin"]);
+            }
+            catch (Exception e)
+            {
+                //查無資料
+                Data = null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            //回傳根據編號所取得的資料
+            return Data;
+        }
+        #endregion
+        #region
+        //確認要註冊帳號是否有被註冊過的方法
+        public bool AccountCheck(string Account)
+        {
+            //藉由導入帳號取得會員資料
+            Members Data = GetDataByAccount(Account);
+            //判斷是否有查詢到會員
+            bool result = (Data == null);
+            //回傳結果
+            return result;
+        }
+
+        #endregion
+    }
+}
